ae368c8b2c3c2ec81430ef6b053c70b9cdf31621
Hiromitsu Yamaguchi <yamagame@mac.com>
2019-05-10 11:34:51 +0900

    切断後の再接続処理を追加

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const conversationId = process.env.SLACK_CHANNEL;
 const configPath = process.env.CONFIG_PATH || path.join(__dirname, 'config.json');
 let started = false;
 let working = true;
+let RTM = null;
 
 function readConfig(configPath) {
   try {
@@ -38,11 +39,11 @@ console.log(config);
 const timer = interval(config.onTime, config.offTime);
 timer.event.on('start', () => {
   working = true;
-  sendMessage(`${timer.onTime}になりましたので開始しました。`, conversationId);
+  if (RTM) RTM.sendMessage(`${timer.onTime}になりましたので開始しました。`, conversationId);
 })
 timer.event.on('end', () => {
   working = false;
-  sendMessage(`${timer.offTime}になりましたので終了しました。`, conversationId);
+  if (RTM) RTM.sendMessage(`${timer.offTime}になりましたので終了しました。`, conversationId);
 })
 timer.start();
 
@@ -50,110 +51,139 @@ function saveConfig(config) {
   fs.writeFileSync(configPath, JSON.stringify(config));
 }
 
-const rtm = new RTMClient(token);
-rtm.start()
-  .catch(console.error);
+function StartRTM() {
+  const rtm = new RTMClient(token);
+  rtm.start()
+    .catch(console.error);
 
-rtm.on('ready', async () => {
-  console.log('Ready');
-  if (!started) {
-    const res = await rtm.sendMessage(`${name}です。起動しました`, conversationId);
+  rtm.on('ready', async () => {
+    console.log('Ready');
+    if (!started) {
+      const res = await rtm.sendMessage(`${name}です。起動しました`, conversationId);
+      console.log('Message sent: ', res.ts);
+      started = true;
+    } else {
+      const res = await rtm.sendMessage(`${name}です。接続しました`, conversationId);
+      console.log('Message sent: ', res.ts);
+    }
+  });
+
+  rtm.on('connected', () => {
+    console.log('connected');
+  })
+
+  rtm.on('reconnecting', () => {
+    console.log('reconnecting');
+  })
+
+  rtm.on('disconnecting', () => {
+    console.log('disconnecting');
+  })
+
+  rtm.on('disconnected', (err) => {
+    console.log(err);
+    console.log('disconnected');
+    RTM = null;
+    setTimeout(() => {
+      RTM = StartRTM();
+    }, 60000)
+  })
+
+  const sendMessage = async (message, channel, callback) => {
+    const res = await rtm.sendMessage(`${name}です。${message}`, channel);
     console.log('Message sent: ', res.ts);
-    started = true;
-  } else {
-    const res = await rtm.sendMessage(`${name}です。接続しました`, conversationId);
-    console.log('Message sent: ', res.ts);
+    if (callback) callback();
   }
-});
 
-const sendMessage = async (message, channel, callback) => {
-  const res = await rtm.sendMessage(`${name}です。${message}`, channel);
-  console.log('Message sent: ', res.ts);
-  if (callback) callback();
+  rtm.on('message', async (event) => {
+    if (event.text.trim().indexOf(name) == 0) {
+      if (event.text.indexOf('元気') >= 0) {
+        sendMessage(`元気です！`, event.channel);
+      } else
+      if (event.text.indexOf('開始時間') >= 0) {
+        const t = event.text.match(/(\d\d):(\d\d)/);
+        if (t) {
+          const time = `${t[1]}:${t[2]}`
+          timer.onTime = time;
+          config.onTime = time;
+          saveConfig(config);
+          sendMessage(`開始時間を${time}に変更しました`, event.channel);
+        } else {
+          sendMessage(`何ですか？`, event.channel);
+        }
+      } else
+      if (event.text.indexOf('終了時間') >= 0) {
+        const t = event.text.match(/(\d\d):(\d\d)/);
+        if (t) {
+          const time = `${t[1]}:${t[2]}`
+          timer.offTime = time;
+          config.offTime = time;
+          saveConfig(config);
+          sendMessage(`終了時間を${time}に変更しました`, event.channel);
+        } else {
+          sendMessage(`何ですか？`, event.channel);
+        }
+      } else
+      if (event.text.indexOf('設定') >= 0) {
+          sendMessage(`開始時間は${timer.onTime}、終了時間は${timer.offTime}です。`, event.channel);
+      } else
+      if (event.text.indexOf('状態') >= 0) {
+          timer.exec('state', (result) => {
+            sendMessage(`${result.trim()}です。`, event.channel);
+          });
+      } else
+      if (event.text.indexOf('開始') >= 0) {
+        working = true;
+        timer.exec('start', () => {
+          sendMessage(`開始しました。`, event.channel);
+        })
+      } else
+      if (event.text.indexOf('終了') >= 0) {
+        working = false;
+        timer.exec('end', () => {
+          sendMessage(`終了しました。`, event.channel);
+        });
+      } else
+      if (event.text.indexOf('何時') >= 0) {
+          sendMessage(`${getTime()}です。`, event.channel);
+      } else
+      if (event.text.indexOf('再起動') >= 0) {
+        sendMessage(`再起動します。`, event.channel, () => {
+          timer.exec('reboot');
+        });
+      } else
+      if (event.text.indexOf('更新') >= 0) {
+        timer.exec('update', (result) => {
+          sendMessage(`${result}`, event.channel);
+        })
+      } else
+      if (event.text.indexOf('IPアドレス') >= 0) {
+        function getIPAddress() {
+          var interfaces = os.networkInterfaces();
+          for (var devName in interfaces) {
+            var iface = interfaces[devName];
+
+            for (var i = 0; i < iface.length; i++) {
+              var alias = iface[i];
+              if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+                return alias.address;
+            }
+          }
+          return '0.0.0.0';
+        }
+        sendMessage(`${getIPAddress()}です。`, event.channel);
+      } else {
+        sendMessage(`何ですか？`, event.channel);
+      }
+    }
+  });
+
+  return {
+    sendMessage,
+  }
 }
 
-rtm.on('message', async (event) => {
-  if (event.text.trim().indexOf(name) == 0) {
-    if (event.text.indexOf('元気') >= 0) {
-      sendMessage(`元気です！`, event.channel);
-    } else
-    if (event.text.indexOf('開始時間') >= 0) {
-      const t = event.text.match(/(\d\d):(\d\d)/);
-      if (t) {
-        const time = `${t[1]}:${t[2]}`
-        timer.onTime = time;
-        config.onTime = time;
-        saveConfig(config);
-        sendMessage(`開始時間を${time}に変更しました`, event.channel);
-      } else {
-        sendMessage(`何ですか？`, event.channel);
-      }
-    } else
-    if (event.text.indexOf('終了時間') >= 0) {
-      const t = event.text.match(/(\d\d):(\d\d)/);
-      if (t) {
-        const time = `${t[1]}:${t[2]}`
-        timer.offTime = time;
-        config.offTime = time;
-        saveConfig(config);
-        sendMessage(`終了時間を${time}に変更しました`, event.channel);
-      } else {
-        sendMessage(`何ですか？`, event.channel);
-      }
-    } else
-    if (event.text.indexOf('設定') >= 0) {
-        sendMessage(`開始時間は${timer.onTime}、終了時間は${timer.offTime}です。`, event.channel);
-    } else
-    if (event.text.indexOf('状態') >= 0) {
-        timer.exec('state', (result) => {
-          sendMessage(`${result.trim()}です。`, event.channel);
-        });
-    } else
-    if (event.text.indexOf('開始') >= 0) {
-      working = true;
-      timer.exec('start', () => {
-        sendMessage(`開始しました。`, event.channel);
-      })
-    } else
-    if (event.text.indexOf('終了') >= 0) {
-      working = false;
-      timer.exec('end', () => {
-        sendMessage(`終了しました。`, event.channel);
-      });
-    } else
-    if (event.text.indexOf('何時') >= 0) {
-        sendMessage(`${getTime()}です。`, event.channel);
-    } else
-    if (event.text.indexOf('再起動') >= 0) {
-      sendMessage(`再起動します。`, event.channel, () => {
-        timer.exec('reboot');
-      });
-    } else
-    if (event.text.indexOf('更新') >= 0) {
-      timer.exec('update', (result) => {
-        sendMessage(`${result}`, event.channel);
-      })
-    } else
-    if (event.text.indexOf('IPアドレス') >= 0) {
-      function getIPAddress() {
-        var interfaces = os.networkInterfaces();
-        for (var devName in interfaces) {
-          var iface = interfaces[devName];
-
-          for (var i = 0; i < iface.length; i++) {
-            var alias = iface[i];
-            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
-              return alias.address;
-          }
-        }
-        return '0.0.0.0';
-      }
-      sendMessage(`${getIPAddress()}です。`, event.channel);
-    } else {
-      sendMessage(`何ですか？`, event.channel);
-    }
-  }
-});
+RTM = StartRTM();
 
 const app = express()
 
